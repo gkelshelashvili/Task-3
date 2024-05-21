33bb45565fe99ae1dd8170c7474a31717cd4f412
gkelshelashvili <giorgikesho6@gmail.com>
2024-05-21 09:56:23 -0800

    Adding files to main branch

---
+++ main.js
@@ -0,0 +1,43 @@
+//calculate
+function calculateSum(num1,num2, ...props) {
+    let sum = 0
+    let sum_2 = 1
+    sum = num1 + num2
+
+    for(const numbers of props) {
+        sum_2 *= numbers
+    }
+
+    return `${sum} შეკრება, ${sum_2} გამრავლება`
+}
+
+console.log(calculateSum(5,5,5,6,5,6,56))
+
+//user obj
+const user = {
+    bank: [
+        {
+            empty: 'empty'
+        },
+        {
+            empty_2: 'empty_2'
+        },
+        {
+          address: {
+            city: 'tbilisi'
+          }  
+        }
+    ]
+}
+//finding user city
+const findCity = ({bank: [, , { address: { city } = {} } ] } = user) =>{
+    return console.log(city)
+} 
+
+findCity()
+
+
+
+
+
+
